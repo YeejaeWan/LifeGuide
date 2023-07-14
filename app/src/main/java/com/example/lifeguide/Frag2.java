2613aed379efb3dcdd921b6546ae95c838a29393
yujinkimkimkim <kimyj4852@naver.com>
2023-07-15 00:35:15 +0900

    상담사 1에서 2 이동

--- app/src/main/java/com/example/lifeguide/Frag2.java
+++ app/src/main/java/com/example/lifeguide/Frag2.java
@@ -1,20 +1,44 @@
 package com.example.lifeguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.lifeguide.frag2_2;
+import com.example.lifeguide.profile_1;
+import com.example.lifeguide.profile_2;
+
 public class Frag2 extends Fragment {
+    Button button2;
+    Button button3;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag2, container, false);
+
+        button2 = view.findViewById(R.id.button2);
+        button3 = view.findViewById(R.id.button3);
+
+        profile_1 p1_1 = new profile_1("이재완 상담가","힘드신 일 있으면 숨기지 마시고 언제든지 말씀해주세요", 4.5);
+        profile_2 p1_2 = new profile_2("남성","이재완 멘토는 연간 50회 이상의 상담을 진행하고 있습니다", "직장 대인 관계 및 업무 스트레스",
+                "이메일, 화상채팅, 전화", "1회 평균 10,000회(상담내용, 시간 별 변동 가능)");
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), frag2_2.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
-
 }
---
+++ app/src/main/java/com/example/lifeguide/profile_2.java
@@ -0,0 +1,75 @@
+package com.example.lifeguide;
+
+public class profile_2 {
+    String s;
+    String activity;
+    String type;
+    String h;
+    String cost;
+    String j;
+    public profile_2(String s, String activity, String type, String h, String cost)
+    {
+        this.s = s;
+        this.activity = activity;
+        this.type = type;
+        this.h = h;
+        this.cost = cost;
+    }
+    public profile_2(String s, String activity, String type, String h, String j , String cost)
+    {
+        this.s = s;
+        this.activity = activity;
+        this.type = type;
+        this.h = h;
+        this.cost = cost;
+        this.j = j;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setJ(String j) {
+        this.j = j;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public void setH(String h) {
+        this.h = h;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String getJ() {
+        return j;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public String getH() {
+        return h;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
