2287e5a07dc67309487a4bb64539492fad19ebca
YeeeeJaeWan <ejw04199@naver.com>
2023-08-03 15:53:48 +0900

    프레그먼트 오류 수정

--- app/src/main/java/com/example/lifeguide/Frag1.java
+++ app/src/main/java/com/example/lifeguide/Frag1.java
@@ -1,5 +1,6 @@
 package com.example.lifeguide;
 
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,7 +8,6 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 public class Frag1 extends Fragment {
     @Nullable
