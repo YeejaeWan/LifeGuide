7564695e1f345e2984050758d2bd49c00bfbaab6
임종호 <imjongho135@gmail.com>
2023-08-19 00:13:04 +0900

    진행중

--- app/src/main/java/com/example/lifeguide/Frag3.java
+++ app/src/main/java/com/example/lifeguide/Frag3.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 public class Frag3 extends Fragment {
     @Nullable
     @Override
--- app/src/main/java/com/example/lifeguide/JonghoTest.java
+++
@@ -1,6 +0,0 @@
-package com.example.lifeguide;
-
-// 빨간색이 아닌 초록색으로 뜨네요
-public class JonghoTest {
-
-}
