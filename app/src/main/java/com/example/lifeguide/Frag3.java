d79b1d085f92ffb9f6c81ade3229d2f90aa107d5
임종호 <imjongho135@gmail.com>
2023-08-20 00:12:21 +0900

    진행중

--- app/src/main/java/com/example/lifeguide/Frag3.java
+++ app/src/main/java/com/example/lifeguide/Frag3.java
@@ -1,9 +1,11 @@
 package com.example.lifeguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +13,30 @@ import androidx.fragment.app.Fragment;
 
 
 public class Frag3 extends Fragment {
+
+    Button button;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag3, container, false);
+
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), ChatRoom.class);
+                startActivity(intent);
+            }
+        });
+
+
         return view;
     }
 
 }
+
+
+
