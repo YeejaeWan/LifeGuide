814c6b88684ec31cb57324343bf2fa794099e29f
임종호 <imjongho135@gmail.com>
2023-08-24 03:54:19 +0900

    진행중

--- app/src/main/java/com/example/lifeguide/Frag3.java
+++ app/src/main/java/com/example/lifeguide/Frag3.java
@@ -23,15 +23,18 @@ class Item {
     final private int image;
     final private String name;
     final private String message;
+    final private String time;
 
     public int getImage(){return image;}
     public String getName(){return name;}
     public String getMessage(){return message;}
+    public String getTime(){return time;}
 
-    public Item(int image, String name, String message){
+    public Item(int image, String name, String message, String time){
         this.image = image;
         this.name = name;
         this.message = message;
+        this.time = time;
     }
 }
 
@@ -82,6 +85,8 @@ class Frag3Adapter extends BaseAdapter {
         TextView message = (TextView)convertView.findViewById(R.id.message);
         message.setText(item.getMessage());
 
+        TextView time = (TextView)convertView.findViewById(R.id.time);
+        time.setText(item.getTime());
 
         return convertView;
     }
@@ -100,13 +105,19 @@ public class Frag3 extends Fragment {
         ListView listviewChat = view.findViewById(R.id.listview_chat);
         ArrayList<Item> data = new ArrayList<>();
 
-        Item person1 = new Item(R.drawable.profile, "이재완 상담사", "안녕하세요.");
-        Item person2 = new Item(R.drawable.profile, "홍현지 멘토", "어떤 부분에서 멘토링을...");
-        Item person3 = new Item(R.drawable.profile, "임종호 상담사", "저번에 했던 상담은 괜찮으...");
+        Item person1 = new Item(R.drawable.profile, "이재완 상담사", "안녕하세요.", "7월 12일");
+        Item person2 = new Item(R.drawable.profile, "홍현지 멘토", "어떤 부분에서 멘토링을...", "오후 1시 14분");
+        Item person3 = new Item(R.drawable.profile, "임종호 상담사", "저번에 했던 상담은 괜찮나요... ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ", "7월 8일");
+        Item person4 = new Item(R.drawable.profile, "이나경 상담사", "처음 뵙습니다~", "8월 10일");
+        Item person5 = new Item(R.drawable.profile, "이찬규 멘토", "상담 결과는 만족스럽나요?", "오전 10시 7분");
+        Item person6 = new Item(R.drawable.profile, "김유진 상담사", "다음에 언제 시간이 되시나요...", "12월 25일");
 
         data.add(person1);
         data.add(person2);
         data.add(person3);
+        data.add(person4);
+        data.add(person5);
+        data.add(person6);
 
         Frag3Adapter adapter = new Frag3Adapter(getActivity(), R.layout.frag3_item, data);
 
